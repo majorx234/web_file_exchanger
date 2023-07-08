976425562a65ad1f5cf46db2b4d5bcc8d3041679
majorx234 <majorx234@googlemail.com>
2023-07-09 01:10:56 +0200

    Fixes: token inited with null, same port as axum server, fix login endpoint,

--- html_test_frontend/index.js
+++ html_test_frontend/index.js
@@ -1,4 +1,4 @@
-var token = Null
+var token = null
 
 function outputToConsole(text) {
     let para = document.createElement("p");
@@ -15,7 +15,7 @@ function httpGet(endpoint_name, variable_context, response_handler, token) {
             response_handler(this.responseText);         
         }
     };
-    let endpoint = "http://" + location.hostname + ":8000/" + endpoint_name;
+    let endpoint = "http://" + location.hostname + ":8080/" + endpoint_name;
     xmlHttp.open("GET", endpoint, true);
 		if (token) {
 				xmlHttp.setRequestHeader('Authorization', 'Bearer ' + token);
@@ -30,7 +30,7 @@ function httpPost(endpoint_name, json_data, variable_context, response_handler, 
             response_handler(this.responseText);
         }
     };
-    let endpoint = "http://" + location.hostname + ":8000/" + endpoint_name;
+    let endpoint = "http://" + location.hostname + ":8080/" + endpoint_name;
     xmlHttp.open("POST", endpoint, true);
     xmlHttp.setRequestHeader('Content-type', 'application/json');
 		if (token) {
@@ -50,19 +50,19 @@ function httpGetTest() {
 }
 
 function httpPostLogin() {
-    let end_point_name = "session/login";
+    let end_point_name = "login";
     let variable_context = "login: ";
     let user_name = document.getElementById("user_name_input").value;
 		let password = document.getElementById("password_input").value;
 		let password_hash = forge_sha256(password + "salt29562");
 		let json_data = {
 				user_name: user_name,
-				password: password_hash
+				password_hash: password_hash
 		};
     let param_name = "login";
     let response_handler = (response_text) => {
         let json_data = JSON.parse(response_text);
-        outputToConsole(variable_context);
+        outputToConsole(variable_context + response_text);
     };
     httpPost(end_point_name, json_data, variable_context, response_handler);
 }
@@ -82,3 +82,5 @@ document.getElementById("get_test_button").onclick = function() {
 document.getElementById("login_button").onclick = function() {
     httpPostLogin();
 };
+
+outputToConsole("init successful");
