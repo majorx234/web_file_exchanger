c9977ec884bd6ade08d5c9706b4095dd96fbecdc
majorx234 <majorx234@googlemail.com>
2023-07-09 02:40:59 +0200

    Refactor static web endpoint to use Config object

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,5 @@
 use std::net::SocketAddr;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 #[derive(Clone, Debug)]
@@ -6,6 +7,7 @@ pub struct Config {
     pub host_ip: String,
     pub port: u32,
     pub database_url: String,
+    pub frontend_dir_path: PathBuf,
 }
 
 impl Config {
@@ -13,14 +15,27 @@ impl Config {
         let host_ip = std::env::var("HOST_IP").expect("HOST_IP not set");
         let port = std::env::var("PORT").expect("PORT not set");
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
-
+        let frontend_dir = std::env::var("FRONTEND_DIR").expect("FRONTEND_DIR not set");
+        let mut frontend_dir_path = PathBuf::new();
+        frontend_dir_path.push(frontend_dir);
+        match frontend_dir_path.is_absolute() {
+            true => (),
+            false => {
+                frontend_dir_path = std::env::current_dir().unwrap().join(frontend_dir_path);
+            }
+        };
         Config {
             host_ip,
             port: port.parse::<u32>().unwrap(),
             database_url,
+            frontend_dir_path,
         }
     }
     pub fn get_host_socket_addr(&self) -> SocketAddr {
         SocketAddr::from_str(&format!("{}:{}", self.host_ip, self.port)[..]).unwrap()
+    }
+
+    pub fn get_frontend_dir_path(&self) -> &Path {
+        self.frontend_dir_path.as_path()
     }
 }
--- src/routers/static_web_page.rs
+++ src/routers/static_web_page.rs
@@ -1,3 +1,4 @@
+use crate::config::Config;
 use axum::routing::get_service;
 use axum::{
     extract::{Extension, Query},
@@ -9,15 +10,7 @@ use std::path::PathBuf;
 use tower_http::services::ServeDir;
 
 pub fn frontend() -> Router {
-    let mut frontend_dir_path = PathBuf::new();
-    frontend_dir_path.push(std::env::var("FRONTEND_DIR").expect("FRONTEND_DIR not set"));
-    match frontend_dir_path.is_absolute() {
-        true => Router::new().nest_service("/", get_service(ServeDir::new(frontend_dir_path))),
-        false => Router::new().nest_service(
-            "/",
-            get_service(ServeDir::new(
-                std::env::current_dir().unwrap().join(frontend_dir_path),
-            )),
-        ),
-    }
+    let config = Config::new();
+    let frontend_dir_path = config.get_frontend_dir_path();
+    Router::new().nest_service("/", get_service(ServeDir::new(frontend_dir_path)))
 }
