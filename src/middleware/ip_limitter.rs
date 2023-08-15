c851543136f3c7d1fab2c6b060d4b130cf74ac02
majorx234 <majorx234@googlemail.com>
2023-08-16 00:00:32 +0200

    Add ip limit from str frunction

--- src/middleware/ip_limitter.rs
+++ src/middleware/ip_limitter.rs
@@ -5,12 +5,13 @@ use crate::{
 use async_trait::async_trait;
 use axum::{extract::FromRequestParts, http::Request, middleware::Next, response::Response};
 use axum_client_ip::{InsecureClientIp, SecureClientIp, SecureClientIpSource};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 #[derive(Clone, Debug)]
-struct IpLimitter {
+pub struct IpLimitter {
     ip_prefix: String,
-    ip_whitelist: Option<String>,
-    ip_blacklist: Option<String>,
+    ip_whitelist: Option<Vec<String>>,
+    ip_blacklist: Option<Vec<String>>,
 }
 
 impl IpLimitter {
@@ -18,6 +19,20 @@ impl IpLimitter {
         Self {
             ip_prefix: String::new(),
             ip_whitelist: None,
+            ip_blacklist: None,
+        }
+    }
+
+    pub fn create_iplimit_from_str(ip_whitelist_str: &str) -> Self {
+        let mut ip_whitelist = Vec::new();
+        for ip in ip_whitelist_str.split(',') {
+            if ip.parse::<IpAddr>().unwrap().is_ipv4() {
+                ip_whitelist.push(ip.to_string());
+            }
+        }
+        Self {
+            ip_prefix: String::new(),
+            ip_whitelist: Some(ip_whitelist),
             ip_blacklist: None,
         }
     }
