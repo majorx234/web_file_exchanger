fd209b81f2289ae31ed4c3d324bbe306d46d7f9a
majorx234 <majorx234@googlemail.com>
2023-08-21 23:55:41 +0200

    Add test with expired token

--- tests/getpost_helper.rs
+++ tests/getpost_helper.rs
@@ -47,5 +47,20 @@ async fn getpost_helper() -> Result<()> {
 
     //hc.do_get("/files").await?.print().await?;
 
+    // test deny old token
+    let old_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiSGVpbnoiLCJleHAiOjE2OTI2NTQ0ODJ9.ED_Ymwe2rxkqCXhbYXv3_pAAysxSy7_2hNt4fpq0G84";
+
+    let bearer_old_token = "Bearer ".to_string() + old_token;
+    let url_files = "http://localhost:8080/files".to_string();
+    let reqwest_res_files = client
+        .get(&url_files)
+        .header("Authorization", &bearer_old_token)
+        .send()
+        .await?;
+    let status = reqwest_res_files.status();
+
+    let json_body = reqwest_res_files.json::<Value>().await?;
+    println!("status: {}, files: {}", status, json_body);
+
     Ok(())
 }
